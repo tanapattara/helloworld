e3f900cded140b5273dda48684c7cf88ed6e95f7
skanjaruek <tanapattara@kku.ac.th>
2021-12-16 11:21:12 +0700

    add c to f

---
+++ TemperatureCalculator/TemperatureCalculator/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TemperatureCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.textBoxF = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(210, 21);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(90, 28);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "C";
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(28, 23);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(120, 26);
+            this.textBoxC.TabIndex = 3;
+            // 
+            // textBoxF
+            // 
+            this.textBoxF.Location = new System.Drawing.Point(28, 55);
+            this.textBoxF.Name = "textBoxF";
+            this.textBoxF.Size = new System.Drawing.Size(120, 26);
+            this.textBoxF.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "F";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 99);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxF);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonConvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button buttonConvert;
+        private Label label1;
+        private TextBox textBoxC;
+        private TextBox textBoxF;
+        private Label label2;
+        private Button button1;
+    }
+}
---
+++ TemperatureCalculator/TemperatureCalculator/Form1.cs
@@ -0,0 +1,25 @@
+﻿namespace TemperatureCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            // get input form textbox
+            string input = textBoxC.Text;
+           
+            // convert to f
+            // f = c × 9/5 + 32
+            double c = Convert.ToDouble(input);
+            double f = c * 9 / 5 + 32;
+
+            // show input to textbox
+            textBoxF.Text = f.ToString();
+            
+        }
+    }
+}
